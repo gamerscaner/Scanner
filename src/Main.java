7ae77528ccc65a3d3eda97c679873e93b7f83184
student12 <student12@gmail.com>
2019-03-16 16:48:35 +0400

    Добавлен ввод целых чисел

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner("Строка1\nСтрока2\n");
+       Scanner scan = new Scanner(System.in);
 
-           String str =  scan.nextLine();
-           String str2 = scan.nextLine();
-           System.out.println(str2);
+       System.out.println("Введите a:");
+       int a = scan.nextInt();
+
+       System.out.println("Введите b:");
+       int b = scan.nextInt();
+
+       System.out.println("Результат: "+(a+b));
+
+
 
 
     }
